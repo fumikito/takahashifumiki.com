b5d13d8d36cc03e4e91f5d6caf10cbaa87b27f75
fumikito <fumikito@yahoo.co.jp>
2011-08-10 16:58:12 +0000

    404できた closes #759 @0.5h

--- 404.php
+++ 404.php
@@ -1,20 +1,13 @@
 <?php
-header("HTTP/1.0 404 Not Found");
-get_header();
+get_header("meta");
+get_header("navi");
+get_header("title");
 ?>
 
-<div id="error404">
-<a rel="home" href="<?php bloginfo('url'); ?>" class="logo"><img alt="高橋文樹.com" src="<?php echo $fumiki->template; ?>/img/body_h1.gif"></a>
-<div class="container">
-<h1>
-	<span>404: Page Not Found.</span><br />
-	<small>お探しのページは見つかりませんでした。</small>
-</h1>
-<form action="<?php echo $fumiki->root; ?>" method="get" id="search" name="search">
-	<div id="s_form">
-		<input id="s" name="s" type="text" value="&raquo;検索語句" /><input id="submit" type="image" name="submit" alt="検索する" src="<?php echo $fumiki->template; ?>/img/body_btn_search.gif" value="検索する" />
+<div id="content" class="margin account" class="clearfix">
+	<div class="entry">
+		<h2>404 Not Found</h2>
+		<p>ページ右上の検索フォームでお探しいただくか、<a href="#footer-nav">ページ下部</a>のメニュー一覧をご利用ください。</p>
 	</div>
-</form>
 </div>
-</div>
-<?php get_footer(); ?>
+<?php get_footer();
