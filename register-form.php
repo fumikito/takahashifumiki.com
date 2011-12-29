c52bd04092265a8be44330944f1092dbb57e9597
fumikito <fumikito@yahoo.co.jp>
2011-12-29 04:35:40 +0000

    登録フォームはテーブル使えず。 refs #1074 @0.1h

--- register-form.php
+++ register-form.php
@@ -11,21 +11,17 @@ Theme My Login will always look in your theme's directory first, before using th
 	<?php $template->the_errors(); ?>
     <form class="registerform" name="registerform" id="registerform<?php $template->the_instance(); ?>" action="<?php $template->the_action_url( 'register' ); ?>" method="post" onsubmit="(function($){$('input[name=user_login]').val($('input[name=user_email]').val());})(jQuery);">
 		<input type="hidden" name="user_login" id="user_login<?php $template->the_instance(); ?>" value="<?php $template->the_posted_value( 'user_login' ); ?>" />
-		<table class="form-table">
-			<tbody>
-				<tr>
-					<th><label for="user_email<?php $template->the_instance(); ?>">メールアドレス</label></th>
-					<td><input type="<?php attr_email(); ?>" name="user_email" id="user_email<?php $template->the_instance(); ?>" class="input" value="<?php $template->the_posted_value( 'user_email' ); ?>" size="20" /></td>
-				</tr>
-			</tbody>
-		</table>
+		<p>
+			<label for="user_email<?php $template->the_instance(); ?>">メールアドレス:</label>
+			<input type="<?php attr_email(); ?>" name="user_email" id="user_email<?php $template->the_instance(); ?>" class="input" value="<?php $template->the_posted_value( 'user_email' ); ?>" size="20" />
+		</p>
 		<?php do_action_ref_array( 'tml_register_form', array( &$template ) ); //TML hook ?>
 		<?php do_action( 'register_form' ); // Wordpress hook ?>
-		<p class="center">
-			<input class="button-primary" type="submit" name="wp-submit" id="wp-submit<?php $template->the_instance(); ?>" value="<?php _e( 'Register', 'theme-my-login' ); ?>" />
+        <p class="center">
+            <input class="button-primary" type="submit" name="wp-submit" id="wp-submit<?php $template->the_instance(); ?>" value="<?php _e( 'Register', 'theme-my-login' ); ?>" />
 			<input type="hidden" name="redirect_to" value="<?php $template->the_redirect_url( 'register' ); ?>" />
 			<input type="hidden" name="instance" value="<?php $template->the_instance(); ?>" />
-		</p>
+        </p>
     </form>
 	<?php $template->the_action_links( array( 'register' => false ) ); ?>
 </div>
