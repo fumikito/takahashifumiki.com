464a62daa7de0dd4c1aeca0ad2a623d1e4732b43
fumikito <guy@hametuha.com>
2017-05-29 19:48:09 +0900

    Facebookインスタント記事の広告コードを変更

--- templates/instant-article.php
+++ templates/instant-article.php
@@ -16,6 +16,7 @@
 		return $cat->name;
 	}, $category) ) ) ?>">
 	<?php endif; ?>
+	<meta property="fb:use_automatic_ad_placement" content="enable=true ad_density=default">
 </head>
 <body>
 <article>
@@ -58,7 +59,7 @@
 	<figure class="op-tracker">
 		<iframe hidden>
 			<script>
-				(function (i,s,o,g,r,a,m) {i['GoogleAnalyticsObject']=r;i[r]=i[r]||function () {(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),                         m=s.getElementsByTagName(o)0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+				(function (i,s,o,g,r,a,m) {i['GoogleAnalyticsObject']=r;i[r]=i[r]||function () {(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 				ga('create', 'UA-5329295-4', 'auto');
 				ga('require', 'displayfeatures');
 				ga('set', 'campaignSource', 'Facebook');
