45a1d3aeb56e6c7a877d5350141d75388b7fe893
fumikito <guy@hametuha.com>
2018-08-08 13:59:59 +0900

    Change contact form.

--- functions/newsletter.php
+++ functions/newsletter.php
@@ -23,29 +23,35 @@ add_shortcode( 'mailchimp', function ( $atts = [], $contnet = '' ) {
                 <div class="form-group">
                     <label for="mce-EMAIL">メール <span class="asterisk text-danger">*</span>
                     </label>
-                    <input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL" placeholder="ex. info@takahashifumiki.com">
+                    <input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL" placeholder="e.g. info@takahashifumiki.com">
                 </div>
                 <div class="form-group">
                     <label for="mce-FNAME">お名前 </label>
-                    <input type="text" value="" name="FNAME" class="form-control" id="mce-FNAME" placeholder="ex. 高橋文樹">
+                    <input type="text" value="" name="FNAME" class="form-control" id="mce-FNAME" placeholder="e.g. 高橋文樹">
                 </div>
                 <div class="form-group">
                     <label for="mce-MMERGE2">会社・団体 </label>
-                    <input type="text" value="" name="MMERGE2" class="form-control" id="mce-MMERGE2" placeholder="ex. 株式会社破滅派">
+                    <input type="text" value="" name="MMERGE2" class="form-control" id="mce-MMERGE2" placeholder="e.g. 株式会社破滅派">
                 </div>
                 <div class="form-group">
 					<div class="radio">
 						<label for="mce-MMERGE3-0"><input type="radio" value="出版関連" name="MMERGE3" id="mce-MMERGE3-0">出版関連</label>
 					</div>
 					<div class="radio">
-						<label for="mce-MMERGE3-1"><input type="radio" value="Web関連" name="MMERGE3" id="mce-MMERGE3-1">Web関連</label>
+						<label for="mce-MMERGE3-1"><input type="radio" value="編集者" name="MMERGE3" id="mce-MMERGE3-1">編集者</label>
 					</div>
 					<div class="radio">
-						<label for="mce-MMERGE3-2"><input type="radio" value="学生" name="MMERGE3" id="mce-MMERGE3-2">学生</label>
+						<label for="mce-MMERGE3-2"><input type="radio" value="作家・ライター" name="MMERGE3" id="mce-MMERGE3-2">作家・ライター</label>
 					</div>
 					<div class="radio">
-						<label for="mce-MMERGE3-3"><input type="radio" value="その他" name="MMERGE3" id="mce-MMERGE3-3">その他</label>
+						<label for="mce-MMERGE3-3"><input type="radio" value="Web関連" name="MMERGE3" id="mce-MMERGE3-3">Web関連</label>
 					</div>
+                    <div class="radio">
+                        <label for="mce-MMERGE3-3"><input type="radio" value="学生" name="MMERGE3" id="mce-MMERGE3-4">学生</label>
+                    </div>
+                    <div class="radio">
+                        <label for="mce-MMERGE3-3"><input type="radio" value="その他" name="MMERGE3" id="mce-MMERGE3-3">その他</label>
+                    </div>
                 </div>
                 <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 				<div style="position: absolute; left: -5000px;" aria-hidden="true">
